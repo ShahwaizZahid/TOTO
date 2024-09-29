6fcea4d0930498d26f67bfef57566ceedd346bf5
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-09-29 18:01:58 +0500

    add: loaction , description ui

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:client/components/my_current_loaction.dart';
+import 'package:client/components/my_description_box.dart';
 import 'package:client/components/my_drawer.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:client/components/my_silver_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,9 +15,31 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home'),
-      centerTitle: true,),
+      backgroundColor: Theme.of(context).colorScheme.background,
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+         MySilverAppBar(
+            title: Text('Title'),
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary
+                ),
+                MyCurrentLoaction(),
+                MyDescriptionBox()
+              ],
+            ), // Pass a String instead of a Text widget
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+          // Add your content here
+        ),
+      ),
     );
   }
 }
