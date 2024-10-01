05072e5c5a777ae479e8b1075bc5784bdb926d31
ShahwaizZahid <shahwaizmughal940@gmail.com>
2024-10-01 23:11:36 +0500

    refactor: styling the menu card

--- client/lib/components/my_food_tile.dart
+++ client/lib/components/my_food_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 import '../models/food.dart';
 
@@ -14,24 +15,46 @@ class MyFoodTile extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         GestureDetector(
-          onTap: (){},
-          child: Row(
-            children: [
-              Expanded(child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                Text(food.name),
-                Text(food.price.toString()),
-                Text(food.description)
-              ],)),
-
-              SizedBox(
-                  height: 120,
-                  width: 120,
-                  child: Image.asset(food.imagePath, height: 120,width: 120,))
-            ],
-            
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(food.name),
+                    Text(
+                      '\$' + food.price.toString(),
+                      style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(food.description,
+                        style: TextStyle(
+                            color:
+                                Theme.of(context).colorScheme.inversePrimary))
+                  ],
+                )),
+                SizedBox(
+                  width: 15,
+                ),
+                ClipRRect(
+                  borderRadius:
+                      BorderRadius.circular(8.0), // Specify the radius value
+                  child: Image.asset(food.imagePath, height: 120),
+                )
+              ],
+            ),
           ),
+        ),
+        Divider(
+          color: Theme.of(context).colorScheme.tertiary,
+          endIndent: 25,
+          indent: 25,
         )
       ],
     );
